32b2d2c467c2911ca9f9274086d37bf0528e507c
rwen <rwen@netbase.com>
2022-02-18 16:29:26 +0800

    fix: left only Greeting example at the beginning

--- src/App.js
+++ src/App.js
@@ -1,30 +1,30 @@
 import './App.css'
 
 import Greeting from './Greeting'
-import Counter from './Counter'
-import RecordsList from './RecordsList'
-import PollingExample from './PollingExample'
-import LastNameWithContext from './LastNameWithContext'
-import LastNameWithoutContext from './LastNameWithoutContext'
-import { SnackbarContextProvider, Snackbar } from './SnackbarContext'
-import Ad from './Ad'
-import CheckInForm from './CheckInForm'
+// import Counter from './Counter'
+// import RecordsList from './RecordsList'
+// import PollingExample from './PollingExample'
+// import LastNameWithContext from './LastNameWithContext'
+// import LastNameWithoutContext from './LastNameWithoutContext'
+// import { SnackbarContextProvider, Snackbar } from './SnackbarContext'
+// import Ad from './Ad'
+// import CheckInForm from './CheckInForm'
 
 const App = () => {
   return (
-    <SnackbarContextProvider>
+    // <SnackbarContextProvider>
       <div className="app">
         <Greeting userName="CodeSandbox" />
-        <Counter />
-        <CheckInForm />
-        <RecordsList />
-        <PollingExample />
-        <LastNameWithContext />
-        <LastNameWithoutContext />
-        <Snackbar />
-        <Ad />
+        {/* <Counter /> */}
+        {/* <CheckInForm /> */}
+        {/* <RecordsList /> */}
+        {/* <PollingExample /> */}
+        {/* <LastNameWithContext /> */}
+        {/* <LastNameWithoutContext /> */}
+        {/* <Snackbar /> */}
+        {/* <Ad /> */}
       </div>
-    </SnackbarContextProvider>
+    // </SnackbarContextProvider>
   )
 }
 
